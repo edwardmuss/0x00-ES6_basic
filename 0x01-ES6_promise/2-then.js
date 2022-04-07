fd27288083378599dbfbf7c5c849f2a46cc9362a
edwardmuss <edwardmuss5@gmail.com>
2022-04-07 15:34:16 +0300

    do some corrections

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }, (err) => err);
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
+
+export default handleResponseFromAPI;
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleSignUp() {
-  Promise.all([uploadPhoto(), createUser()]).then(
-    (results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    }, () => {
-      console.log('Signup system offline');
-    },
-  );
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;
