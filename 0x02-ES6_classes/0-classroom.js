8378ea7df2fc4c5927731aab9fbac97cb889e04f
edwardmuss <edwardmuss5@gmail.com>
2022-04-07 15:19:17 +0300

    some fixes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
-export default class ClassRoom{
-
+export default class ClassRoom {
     constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize;
-      }
-}
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
+  
